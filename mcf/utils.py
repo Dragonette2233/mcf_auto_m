3ffa3fe187bb14df6d6363306402da2ab243c2d0
DragonettE <rodanban251@gmail.com>
2024-09-29 12:53:46 +0300

    fix f-str

--- mcf/utils.py
+++ mcf/utils.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 def is_riot_apikey_valid():
     
-    res = requests.get(f"https://euw1.api.riotgames.com/lol/spectator/v5/featured-games?api_key={uStorage.get_key("RIOT_API")}")
+    res = requests.get(f"https://euw1.api.riotgames.com/lol/spectator/v5/featured-games?api_key={uStorage.get_key('RIOT_API')}")
 
     if res.status_code == 403:
         logger.error("Riot API key invalid")
