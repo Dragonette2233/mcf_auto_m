f0006586a7f94ee1b283b6f8c08b717aea4a25f0
Porosenok <hearthearthead1771@gmail.com>
2025-05-08 13:10:22 +0300

    oserror handle

--- mcf/utils.py
+++ mcf/utils.py
@@ -8,15 +8,19 @@ PROXIES = uStorage.get_key("PROXIES")
 
 
 def is_riot_apikey_valid():
-    
-    res = requests.get(f"https://euw1.api.riotgames.com/lol/spectator/v5/featured-games?api_key={uStorage.get_key('RIOT_API')}", verify=False, proxies=PROXIES)
+    try:
+        res = requests.get(f"https://euw1.api.riotgames.com/lol/spectator/v5/featured-games?api_key={uStorage.get_key('RIOT_API')}", verify=False, proxies=PROXIES)
 
-    if res.status_code == 403:
-        logger.error("Riot API key invalid")
-        return False
-    
-    logger.info('Riot API key is correct!')
-    return True
+        if res.status_code == 403:
+            logger.error("Riot API key invalid")
+            return False
+        
+        logger.info('Riot API key is correct!')
+        return True
+    except OSError:
+        logger.warning("Connection failed. Skipping")
+    except Exception as e:
+        logger.warning(e)
 
 def extract_code_and_area(summoner_name: str) -> tuple:
     
