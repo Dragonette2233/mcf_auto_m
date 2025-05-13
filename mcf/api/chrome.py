ecc2b60d08a5c173bc918360237d249321abe8dc
MegMyers1771 <110187774+MegMyers1771@users.noreply.github.com>
2025-05-13 14:26:37 +0300

    ,,,

--- mcf/api/chrome.py
+++ mcf/api/chrome.py
@@ -133,8 +133,8 @@ class Chrome:
                         self.ACTIVE_TOTAL_VALUE = total_value
                         # print(total_value)
                         lock_ico = mrk.find_elements(By.CSS_SELECTOR, MelCSS.LOCK_ICON)
-                        if len(lock_ico) == 0:
-                            return True
+                        lock_icon_svg = markets[0].find_elements(By.CSS_SELECTOR, MelCSS.LOCK_ICON_SVG)
+                        if len(lock_ico) == 0 or len(lock_icon_svg) == 0: return True
         except:
             ...
             
